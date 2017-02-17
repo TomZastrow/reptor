00cc9bc0c3aa770df9044e32bf052973bd866e31
post <post@192.168.0.11>
2017-02-17 22:16:07 +0100

    Finishing collection API

--- collections/api.php
+++ collections/api.php
@@ -1,7 +1,8 @@
 <?php
-include("../generalFunctions.php");
-$method = $_SERVER['REQUEST_METHOD'];
 
+include("../generalFunctions.php");
+
+$method = $_SERVER['REQUEST_METHOD'];
 $request = $_SERVER['REQUEST_URI'];
 
 $apipos = strpos($request, "api.php");
@@ -10,60 +11,44 @@ $request = str_replace("api.php/", "", $request);
 $request = rtrim($request, "/");
 $request = ltrim($request, "/");
 
-//echo "Request: $request \n";
-
-$rec = split("/", $request);
+$rec = explode("/", $request);
 $path = "";
 
 if ($rec[sizeof($rec) - 1] == "members") {
-    echo "LAST MEMBER";
-    for($i = 0; $i < sizeof($rec)-1; $i++){
+    for ($i = 0; $i < sizeof($rec) - 1; $i++) {
         $path = $path . $rec[$i] . "/";
     }
 } elseif ($rec[sizeof($rec) - 2] == "members") {
-    echo "SECOND LAST MEMBER";
-    for($i = 0; $i < sizeof($rec)-2; $i++){
+    for ($i = 0; $i < sizeof($rec) - 2; $i++) {
         $path = $path . $rec[$i] . "/";
     }
 } else {
-    for($i = 0; $i < sizeof($rec); $i++){
+    for ($i = 0; $i < sizeof($rec); $i++) {
         $path = $path . $rec[$i] . "/";
-    }    
+    }
 }
 
 $path = str_replace("collections", "../data", $path);
-//$path = str_replace("/members", "", $path);
 
-
-//echo "Path: $path \n";
-//echo "REC: " . sizeof($rec) . "\n";
-//echo var_dump($rec) . "\n";
-
-$verb = "??";
-
-echo "PATH: " . $path . "\n";
-
+// --- List of all collections: curl -X GET http://localhost:8000/collections/api.php/collections
 if (sizeof($rec) == 1 && $rec[0] == "collections") {
-            $objects = new RecursiveIteratorIterator(new RecursiveDirectoryIterator("../data"), RecursiveIteratorIterator::SELF_FIRST);
-        $result = "";
-        
-        foreach ($objects as $name) {
-            
-                
-                //if (file_exists($name . "collection.txt")) {
-            if( strpos(  $name->getPathname(), "collection.txt")){
-                $temp = str_replace("../data", "", $name);
-                $temp = str_replace("collection.txt", "", $temp);
-                $temp = str_replace("\\", "/", $temp);
-                 $result = $result . $temp . "\n";
-                }
-        
+    $objects = new RecursiveIteratorIterator(new RecursiveDirectoryIterator("../data"), RecursiveIteratorIterator::SELF_FIRST);
+    $result = "";
+
+    foreach ($objects as $name) {
+        if (strpos($name->getPathname(), "collection.txt")) {
+            $temp = str_replace("../data", "", $name);
+            $temp = str_replace("collection.txt", "", $temp);
+            $temp = str_replace("\\", "/", $temp);
+            $result = $result . $temp . "\n";
         }
-        echo "$result";
-    $verb = "GetListOfCollections";
+    }
+    echo "$result";
 }
 
+// --- Dealing with members:
 if (sizeof($rec) > 1 && $rec[sizeof($rec) - 1] === "members") {
+    // --- Adding a new member to a collecltion: curl -X POST -d '{"id" : "abc"}'  http://localhost:8000/collections/api.php/collections/Photos/members
     if ($method == "POST") {
         $postdata = file_get_contents("php://input");
         $data = json_decode($postdata, true);
@@ -71,21 +56,24 @@ if (sizeof($rec) > 1 && $rec[sizeof($rec) - 1] === "members") {
         $collectionHandle = fopen($path . "/collection.txt", "a") or die("Unable to open file!");
         fwrite($collectionHandle, $data["id"] . "\n");
         fclose($collectionHandle);
-        $verb = "AddMember";
         echo '{"success" : "Added ' . $data["id"] . ' to ' . $request . '"}' . "\n";
     }
+    
+    // --- Getting all members of a collection: curl -X GET http://localhost:8000/collections/api.php/collections/Photos/members
     if ($method == "GET") {
         $temp = '{"contents" : [' . "\n";
         foreach (file($path . "collection.txt") as $line) {
             $line = trim($line);
-            $temp = $temp . '{"id" : "' . $line . "\"},\n";
+            if(!$line == ""){
+                $temp = $temp . '{"id" : "' . $line . "\"},\n";
+            }
         }
-        $temp = rtrim(trim($temp), ',') . "]}";
+        $temp = rtrim(trim($temp), ',') . "]}\n";
         echo $temp;
-        $verb = "GetMembers";
     }
 }
 
+// --- Delete a member: curl -X DELETE http://localhost:8000/collections/api.php/collections/Photos/members/123
 if (sizeof($rec) > 1 && $rec[sizeof($rec) - 2] === "members" && $method == "DELETE") {
     $itemToDelete = $rec[sizeof($rec) - 1];
     $collectionFile = $path . "collection.txt";
@@ -105,24 +93,25 @@ if (sizeof($rec) > 1 && $rec[sizeof($rec) - 2] === "members" && $method == "DELE
     } else {
         echo "{\"error\" : \" Collection $collectionFile does not exists\"}\n";
     }
-    $verb = "DeleteMember";
 }
 
+
+// --- Deleting a collection: curl -X DELETE http://localhost:8000/collections/api.php/collections/Photos/Winter/
 if (sizeof($rec) > 1 && $rec[sizeof($rec) - 2] !== "members" && $rec[sizeof($rec) - 1] !== "members" && $method == "DELETE") {
     $collectionFile = $path . "collection.txt";
     echo "COLLFILE: " . $collectionFile;
-    if(file_exists($collectionFile)){
-         unlink($collectionFile);
-                echo "{\"success\" : \" Collection $collectionFile was successfully deleted\"}\n";
-
-        } else {
-                echo "{\"error\" : \" Collection $collectionFile does not exists and cant be deleted\"}\n";
-        }
+    if (file_exists($collectionFile)) {
+        unlink($collectionFile);
+        echo "{\"success\" : \" Collection $collectionFile was successfully deleted\"}\n";
+    } else {
+        echo "{\"error\" : \" Collection $collectionFile does not exists and cant be deleted\"}\n";
+    }
     $verb = "DeleteCollection";
 }
 
+// --- Creating a collection: curl -X POST http://localhost:8000/collections/api.php/collections/xxx
 if (sizeof($rec) > 1 && $rec[sizeof($rec) - 2] !== "members" && $rec[sizeof($rec) - 1] !== "members" && $method == "POST") {
-    $verb = "CreateCollection";
+    
     if (!file_exists($path)) {
         mkdir($path, 0777, true);
         touch($path . "/collection.txt");
@@ -134,5 +123,3 @@ if (sizeof($rec) > 1 && $rec[sizeof($rec) - 2] !== "members" && $rec[sizeof($rec
         echo "{\"error\" : \"Collection exists\"}\n";
     }
 }
-
-//echo "<br><br>Verb: $verb <br>\n";
